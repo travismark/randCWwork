cd126a0e56d3a796480b84de4ffc4ba08865f81d
Travis Baer <tbaer@clockwork-solutions.com>
2016-01-05 17:36:52 -0600

    minor updates to focus on sole taske of writing csvs and plots

--- weibullsWork.R
+++ weibullsWork.R
@@ -1,49 +1,27 @@
-#weibullswork
-#March 1 2013:  moving all my one-off examples from weibulls.R so I can source weibulls.R
+# Create some Weibull Output 
+# March 1 2013:  moving all my one-off examples from weibulls.R so I can source weibulls.R
+# January 2015: updated to use in the new clockworkETL
 
 library("fitdistrplus")
 library("plyr")
-setwd("C:/Users/tbaer/Desktop/cbm/october redo weibulls/R work")
-weibulls_initial<-read.csv("newTOW_LOC.csv",header=TRUE)
-#weibulls_initial<-read.csv("./newTOW_LOC_10.01.2010.NoOptions.csv",header=TRUE)
-weibulls_initial<-read.csv("./newTOW_LOC_diffOrder.csv",header=TRUE)
 
-source("weibulls.R")
-weibullPN_info<-read.csv("tblWUCinfo.csv",header=TRUE)
+# get script path to get correct relative path for files
+args <- commandArgs(trailingOnly = FALSE)
+scriptPath <- normalizePath(dirname(sub("^--file=", "", args[grep("^--file=", args)])))
+if (interactive()) scriptPath <- getwd()
+setwd(scriptPath)
 
+#setwd("C:/Users/tbaer/Desktop/cbm/october redo weibulls/R work")
+#weibulls_initial<-read.csv("newTOW_LOC.csv",header=TRUE)
+weibulls_initial<-read.csv("./newTOW_LOC.csv"),header=TRUE)
+
+source("./weibulls.R")
+weibullPN_info<-read.csv("./tblWUCinfo.csv",header=TRUE)
 
 system.time(allweibulls<- gatherallweibulls(weibulls_initial,
-                                            plot=FALSE,modkm=TRUE,plotdir="./plots/")) #ALL THE WEIBULLz
+                                            plot=FALSE,modkm=TRUE,plotdir="./plots/")) #ALL THE WEIBULL
 write.csv(allweibulls,file="Apache Weibulls.csv",quote=FALSE)
 
-subdf<-weibulls_initial[weibulls_initial$WUC=="06A" & weibulls_initial$PN=="7-311310001-43" ,]
-subdf<-droplevels(subdf)
-censsubdf<-CensUncens(subdf)
-weibsubdf<-fitdistcens(censsubdf,"weibull")
-
-ranksubdf<-getModKM(censsubdf,weibsubdf[[1]])
-#getPlotFromDF(ranksubdf,subdf,weibsubdf[[1]],"WUCadjRepInt")
-
-
-getwd()
-setwd("./plots")
-setwd("..")
-getwd()
-
-source("stattest.R")
+source("./stattest.R")
 system.time(alltests<-testallweibulls(allweibulls,weibulls_initial,doAllGroups=FALSE))
 write.csv(alltests,file="Apache Tests.csv",quote=FALSE)
-
-b<-0
-for(ii in 2:(11-1)) {
-  b<-b+dim(combn(11,ii))[2]
-}
-
-# Write up the markdown file and then convert it to HTML
-require(knitr)
-require(markdown)
-knit("C:\\Users\\tbaer\\Desktop\\cbm\\october redo weibulls\\R work\\Rdocumentation.Rmd")
-knit2html("C:\\Users\\tbaer\\Desktop\\cbm\\october redo weibulls\\R work\\Rdocumentation.Rmd")
-knit2pdf("C:\\Users\\tbaer\\Desktop\\cbm\\october redo weibulls\\R work\\Rdocumentation.Rmd")
-
-spin("C:\\Users\\tbaer\\Desktop\\cbm\\october redo weibulls\\R work\\Rdocumentation.Rmd",format="Rnw")
