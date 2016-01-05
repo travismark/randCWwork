1bf6d1d33792aed642de48fb44b98bf308de2a7a
Travis Baer <tbaer@clockwork-solutions.com>
2016-01-05 17:37:15 -0600

    minor updates to focus on functions and documentation

--- weibull_functions.R
+++ weibull_functions.R
@@ -166,7 +166,6 @@ calloneDDPLY<-function(weibulls_table,include,plots,classifiernames,numericalnam
 } # end calloneDDPLY
 
 
-
 # plot a probability graph plot of the events and their fit 
 #   overlayed with a histogram of the censored values
 # called from within getweibullsfromdf
@@ -340,30 +339,6 @@ findCrow<-function(input,fitprob,nonpar) {
   } # end not the first row
 } # end find C
 
-# testA<-apply(X=as.matrix(ranksubdf),MARGIN=1,FUN=findArow,fitprob=ranksubdf$fitprob)
-# testB<-apply(X=as.matrix(ranksubdf),MARGIN=1,FUN=findBrow,fitprob=ranksubdf$fitprob,nonpar=ranksubdf$p)
-# testC<-apply(X=as.matrix(ranksubdf),MARGIN=1,FUN=findCrow,fitprob=ranksubdf$fitprob,nonpar=ranksubdf$p)
-
-## plot
-# plot(uncens$tow,uncens$yval,log="xy")
-#  http://rtricks.wordpress.com/2009/10/30/decimal-log-scale-on-a-plot/
-
-#log-scale example:
-# op<-par()
-# lx <- seq(1,5, length=41)
-# yl <- expression(e^{-frac(1,2) * {log[10](x)}^2})
-# y <- exp(-.5*lx^2)
-# par(mfrow=c(2,1), mar=par("mar")+c(0,1,0,0))
-# plot(10^lx, y, log="xy", type="l", col="purple",
-#      +      main="Log-Log plot", ylab=yl, xlab="x")
-# plot(10^lx, y, log="xy", type="o", pch='.', col="forestgreen",
-#      +      main="Log-Log plot with custom axes", ylab=yl, xlab="x",
-#      +      axes = FALSE, frame.plot = TRUE)
-# my.at <- 10^(1:5)
-# axis(1, at = my.at, labels = formatC(my.at, format="fg"))
-# at.y <- 10^(-5:-1)
-# axis(2, at = at.y, labels = formatC(at.y, format="fg"), col.axis="red")
-# par(op)
 
 ###plotting functions###
 #fix the scale to Weibull:
